ca1222994d9f7351d904ccaef3c33b77b1348006
Michael Fehr <javacrypto@gmx.de>
2020-11-07 12:07:56 +0100

    Add files via upload

---
+++ GenerateAesKey/GenerateRandomAesKey.java
@@ -0,0 +1,22 @@
+package GenerateAesKey;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class GenerateRandomAesKey {
+    public static void main(String[] args) {
+        System.out.println("Generate a 32 byte long AES key");
+        byte[] aesKey = generateRandomAesKey();
+        String aesKeyBase64 = base64Encoding(aesKey);
+        System.out.println("generated key length: " + aesKey.length + " base64: " + aesKeyBase64);
+    }
+    private static byte[] generateRandomAesKey() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] key = new byte[32];
+        secureRandom.nextBytes(key);
+        return key;
+    }
+    private static String base64Encoding(byte[] input) {
+        return Base64.getEncoder().encodeToString(input);
+    }
+}

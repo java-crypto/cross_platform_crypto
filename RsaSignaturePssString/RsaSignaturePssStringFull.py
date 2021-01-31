cee6030081fb9c7ea9dddb516cfc742c49386ccb
Michael Fehr <javacrypto@gmx.de>
2021-01-31 12:15:16 +0100

    Add files via upload

---
+++ RsaSignaturePssString/RsaSignaturePssStringFull.py
@@ -0,0 +1,89 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pss
+from Crypto import Hash
+import base64
+
+def base64Encoding(input):
+  dataBase64 = base64.b64encode(input)
+  dataBase64P = dataBase64.decode("UTF-8")
+  return dataBase64P
+
+def base64Decoding(input):
+    return base64.decodebytes(input.encode("ascii"))
+
+def rsaSignToBase64(privateKeyPem, message):
+  rsaPrivateKey = RSA.import_key(privateKeyPem)
+  hashMessage = Hash.SHA256.new(message.encode("ascii"))
+  signature = pss.new(rsaPrivateKey).sign(hashMessage)
+  return base64Encoding(signature)
+
+def rsaVerifySignatureFromBase64(publicKeyPem, message, signatureBase64):
+    rsaPublicKey = RSA.import_key(publicKeyPem)
+    signatureBytes = base64Decoding(signatureBase64)
+    hashMessage = Hash.SHA256.new(message.encode("ascii"))
+    verifier = pss.new(rsaPublicKey, mask_func=lambda x, y: pss.MGF1(x, y, Hash.SHA256), salt_bytes=Hash.SHA256.digest_size)
+    try:
+      verifier.verify(hashMessage, signatureBytes)
+      return True
+    except (ValueError):
+      return False
+
+def loadRsaPrivateKeyPem():
+  return """-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDwSZYlRn86zPi9
+e1RTZL7QzgE/36zjbeCMyOhf6o/WIKeVxFwVbG2FAY3YJZIxnBH+9j1XS6f+ewjG
+FlJY4f2IrOpS1kPiO3fmOo5N4nc8JKvjwmKtUM0t63uFFPfs69+7mKJ4w3tk2mSN
+4gb8J9P9BCXtH6Q78SdOYvdCMspA1X8eERsdLb/jjHs8+gepKqQ6+XwZbSq0vf2B
+MtaAB7zTX/Dk+ZxDfwIobShPaB0mYmojE2YAQeRq1gYdwwO1dEGk6E5J2toWPpKY
+/IcSYsGKyFqrsmbw0880r1BwRDer4RFrkzp4zvY+kX3eDanlyMqDLPN+ghXT1lv8
+snZpbaBDAgMBAAECggEBAIVxmHzjBc11/73bPB2EGaSEg5UhdzZm0wncmZCLB453
+XBqEjk8nhDsVfdzIIMSEVEowHijYz1c4pMq9osXR26eHwCp47AI73H5zjowadPVl
+uEAot/xgn1IdMN/boURmSj44qiI/DcwYrTdOi2qGA+jD4PwrUl4nsxiJRZ/x7PjL
+hMzRbvDxQ4/Q4ThYXwoEGiIBBK/iB3Z5eR7lFa8E5yAaxM2QP9PENBr/OqkGXLWV
+qA/YTxs3gAvkUjMhlScOi7PMwRX9HsrAeLKbLuC1KJv1p2THUtZbOHqrAF/uwHaj
+ygUblFaa/BTckTN7PKSVIhp7OihbD04bSRrh+nOilcECgYEA/8atV5DmNxFrxF1P
+ODDjdJPNb9pzNrDF03TiFBZWS4Q+2JazyLGjZzhg5Vv9RJ7VcIjPAbMy2Cy5BUff
+EFE+8ryKVWfdpPxpPYOwHCJSw4Bqqdj0Pmp/xw928ebrnUoCzdkUqYYpRWx0T7YV
+RoA9RiBfQiVHhuJBSDPYJPoP34kCgYEA8H9wLE5L8raUn4NYYRuUVMa+1k4Q1N3X
+Bixm5cccc/Ja4LVvrnWqmFOmfFgpVd8BcTGaPSsqfA4j/oEQp7tmjZqggVFqiM2m
+J2YEv18cY/5kiDUVYR7VWSkpqVOkgiX3lK3UkIngnVMGGFnoIBlfBFF9uo02rZpC
+5o5zebaDImsCgYAE9d5wv0+nq7/STBj4NwKCRUeLrsnjOqRriG3GA/TifAsX+jw8
+XS2VF+PRLuqHhSkQiKazGr2Wsa9Y6d7qmxjEbmGkbGJBC+AioEYvFX9TaU8oQhvi
+hgA6ZRNid58EKuZJBbe/3ek4/nR3A0oAVwZZMNGIH972P7cSZmb/uJXMOQKBgQCs
+FaQAL+4sN/TUxrkAkylqF+QJmEZ26l2nrzHZjMWROYNJcsn8/XkaEhD4vGSnazCu
+/B0vU6nMppmezF9Mhc112YSrw8QFK5GOc3NGNBoueqMYy1MG8Xcbm1aSMKVv8xba
+rh+BZQbxy6x61CpCfaT9hAoA6HaNdeoU6y05lBz1DQKBgAbYiIk56QZHeoZKiZxy
+4eicQS0sVKKRb24ZUd+04cNSTfeIuuXZrYJ48Jbr0fzjIM3EfHvLgh9rAZ+aHe/L
+84Ig17KiExe+qyYHjut/SC0wODDtzM/jtrpqyYa5JoEpPIaUSgPuTH/WhO3cDsx6
+3PIW4/CddNs8mCSBOqTnoaxh
+-----END PRIVATE KEY-----
+"""
+
+def loadRsaPublicKeyPem():
+  return """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8EmWJUZ/Osz4vXtUU2S+
+0M4BP9+s423gjMjoX+qP1iCnlcRcFWxthQGN2CWSMZwR/vY9V0un/nsIxhZSWOH9
+iKzqUtZD4jt35jqOTeJ3PCSr48JirVDNLet7hRT37Ovfu5iieMN7ZNpkjeIG/CfT
+/QQl7R+kO/EnTmL3QjLKQNV/HhEbHS2/44x7PPoHqSqkOvl8GW0qtL39gTLWgAe8
+01/w5PmcQ38CKG0oT2gdJmJqIxNmAEHkatYGHcMDtXRBpOhOSdraFj6SmPyHEmLB
+ishaq7Jm8NPPNK9QcEQ3q+ERa5M6eM72PpF93g2p5cjKgyzzfoIV09Zb/LJ2aW2g
+QwIDAQAB
+-----END PUBLIC KEY-----"""
+
+print("RSA signature PSS string")
+
+dataToSign = "The quick brown fox jumps over the lazy dog"
+print("dataToSign: " + dataToSign)
+
+print("\n* * * sign the plaintext with the RSA private key * * *")
+privateRsaKeyPem = loadRsaPrivateKeyPem()
+print("used private key:\n" + privateRsaKeyPem)
+signatureBase64 = rsaSignToBase64(privateRsaKeyPem, dataToSign)
+print("signature (Base64): " + signatureBase64)
+
+print("\n* * * verify the signature against the plaintext with the RSA public key * * *")
+publicRsaKeyPem = loadRsaPublicKeyPem()
+print("used public key:\n" + publicRsaKeyPem)
+signatureVerified = rsaVerifySignatureFromBase64(publicRsaKeyPem, dataToSign, signatureBase64)
+print("\nsignature (Base64) verified: ")
+print(signatureVerified)

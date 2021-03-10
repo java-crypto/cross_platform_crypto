b6cb050a8b4f1af7029d932429daf7a5d852805e
Michael Fehr <javacrypto@gmx.de>
2021-03-10 11:19:20 +0100

    Update PqcJavaSikeKem.java

--- PostQuantumCryptography/SikeKem/PqcJavaSikeKem.java
+++ PostQuantumCryptography/SikeKem/PqcJavaSikeKem.java
@@ -30,7 +30,7 @@ public class PqcJavaSikeKem {
                 "* output and other details         *\n" +
                 "*                                  *\n" +
                 "*    DO NOT USE THE PROGRAM IN     *\n" +
-                "*    ANY PRODUCTION ENVIRONENT     *\n" +
+                "*    ANY PRODUCTION ENVIRONMENT    *\n" +
                 "************************************");
 
         // data to encrypt is usually a 32 bytes long (randomly generated) AES key

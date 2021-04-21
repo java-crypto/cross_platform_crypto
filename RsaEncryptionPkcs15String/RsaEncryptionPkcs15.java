0ea435c20f56dbf4c1c9eeff1a0113b1770ac501
Michael Fehr <javacrypto@gmx.de>
2021-04-21 18:17:05 +0200

    Add files via upload

---
+++ RsaEncryptionPkcs15String/RsaEncryptionPkcs15.java
@@ -0,0 +1,147 @@
+package RSA_Encryption_2048_PKCS1_5;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class RsaEncryptionPkcs15 {
+    public static void main(String[] args) throws GeneralSecurityException {
+        System.out.println("RSA 2048 encryption PKCS 1.5 string");
+
+        String dataToEncryptString = "The quick brown fox jumps over the lazy dog";
+        byte[] dataToEncrypt = dataToEncryptString.getBytes(StandardCharsets.UTF_8);
+        System.out.println("plaintext: " + dataToEncryptString);
+
+        // # # # usually we would load the private and public key from a file or keystore # # #
+        // # # # here we use hardcoded keys for demonstration - don't do this in real programs # # #
+        String filenamePrivateKeyPem = "privatekey2048.pem";
+        String filenamePublicKeyPem = "publickey2048.pem";
+
+        // encryption
+        System.out.println("\n* * * encrypt the plaintext with the RSA public key * * *");
+        PublicKey publicKeyLoad = getPublicKeyFromString(loadRsaPublicKeyPem());
+        // use this in production
+        //PublicKey publicKeyLoad = getPublicKeyFromString(loadRsaKeyPemFile(filenamePublicKeyPem));
+        String ciphertextBase64 = base64Encoding(rsaEncryptionPkcs1(publicKeyLoad, dataToEncrypt));
+        System.out.println("ciphertextBase64: " + ciphertextBase64);
+
+        // transport the encrypted data to recipient
+
+        // receiving the encrypted data, decryption
+        System.out.println("\n* * * decrypt the ciphertext with the RSA private key * * *");
+        String ciphertextReceivedBase64 = ciphertextBase64;
+        System.out.println("ciphertextReceivedBase64: " + ciphertextReceivedBase64);
+        PrivateKey privateKeyLoad = getPrivateKeyFromString(loadRsaPrivateKeyPem());
+        // use this in production
+        //PrivateKey privateKeyLoad = getPrivateKeyFromString(loadRsaKeyPemFile(filenamePrivateKeyPem));
+        byte[] ciphertextReceived = base64Decoding(ciphertextReceivedBase64);
+        byte[] decryptedtextByte = rsaDecryptionPkcs1(privateKeyLoad, ciphertextReceived);
+        System.out.println("decryptedtext: " + new String(decryptedtextByte, StandardCharsets.UTF_8));
+    }
+
+    public static byte[] rsaEncryptionPkcs1 (PublicKey publicKey, byte[] plaintextByte) throws NoSuchAlgorithmException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchPaddingException {
+        byte[] ciphertextByte = null;
+        Cipher encryptCipher = Cipher.getInstance("RSA/ECB/PKCS1PADDING");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        ciphertextByte = encryptCipher.doFinal(plaintextByte);
+        return ciphertextByte;
+    }
+
+    public static byte[] rsaDecryptionPkcs1 (PrivateKey privateKey, byte[] ciphertextByte) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte[] decryptedtextByte = null;
+        Cipher decryptCipher = Cipher.getInstance("RSA/ECB/PKCS1PADDING");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+        decryptedtextByte = decryptCipher.doFinal(ciphertextByte);
+        return decryptedtextByte;
+    }
+
+    private static String base64Encoding(byte[] input) {
+        return Base64.getEncoder().encodeToString(input);
+    }
+    private static byte[] base64Decoding(String input) {
+        return Base64.getDecoder().decode(input);
+    }
+
+    private static String loadRsaPrivateKeyPem() {
+        // this is a sample key - don't worry !
+        return "-----BEGIN PRIVATE KEY-----\n" +
+                "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDwSZYlRn86zPi9\n" +
+                "e1RTZL7QzgE/36zjbeCMyOhf6o/WIKeVxFwVbG2FAY3YJZIxnBH+9j1XS6f+ewjG\n" +
+                "FlJY4f2IrOpS1kPiO3fmOo5N4nc8JKvjwmKtUM0t63uFFPfs69+7mKJ4w3tk2mSN\n" +
+                "4gb8J9P9BCXtH6Q78SdOYvdCMspA1X8eERsdLb/jjHs8+gepKqQ6+XwZbSq0vf2B\n" +
+                "MtaAB7zTX/Dk+ZxDfwIobShPaB0mYmojE2YAQeRq1gYdwwO1dEGk6E5J2toWPpKY\n" +
+                "/IcSYsGKyFqrsmbw0880r1BwRDer4RFrkzp4zvY+kX3eDanlyMqDLPN+ghXT1lv8\n" +
+                "snZpbaBDAgMBAAECggEBAIVxmHzjBc11/73bPB2EGaSEg5UhdzZm0wncmZCLB453\n" +
+                "XBqEjk8nhDsVfdzIIMSEVEowHijYz1c4pMq9osXR26eHwCp47AI73H5zjowadPVl\n" +
+                "uEAot/xgn1IdMN/boURmSj44qiI/DcwYrTdOi2qGA+jD4PwrUl4nsxiJRZ/x7PjL\n" +
+                "hMzRbvDxQ4/Q4ThYXwoEGiIBBK/iB3Z5eR7lFa8E5yAaxM2QP9PENBr/OqkGXLWV\n" +
+                "qA/YTxs3gAvkUjMhlScOi7PMwRX9HsrAeLKbLuC1KJv1p2THUtZbOHqrAF/uwHaj\n" +
+                "ygUblFaa/BTckTN7PKSVIhp7OihbD04bSRrh+nOilcECgYEA/8atV5DmNxFrxF1P\n" +
+                "ODDjdJPNb9pzNrDF03TiFBZWS4Q+2JazyLGjZzhg5Vv9RJ7VcIjPAbMy2Cy5BUff\n" +
+                "EFE+8ryKVWfdpPxpPYOwHCJSw4Bqqdj0Pmp/xw928ebrnUoCzdkUqYYpRWx0T7YV\n" +
+                "RoA9RiBfQiVHhuJBSDPYJPoP34kCgYEA8H9wLE5L8raUn4NYYRuUVMa+1k4Q1N3X\n" +
+                "Bixm5cccc/Ja4LVvrnWqmFOmfFgpVd8BcTGaPSsqfA4j/oEQp7tmjZqggVFqiM2m\n" +
+                "J2YEv18cY/5kiDUVYR7VWSkpqVOkgiX3lK3UkIngnVMGGFnoIBlfBFF9uo02rZpC\n" +
+                "5o5zebaDImsCgYAE9d5wv0+nq7/STBj4NwKCRUeLrsnjOqRriG3GA/TifAsX+jw8\n" +
+                "XS2VF+PRLuqHhSkQiKazGr2Wsa9Y6d7qmxjEbmGkbGJBC+AioEYvFX9TaU8oQhvi\n" +
+                "hgA6ZRNid58EKuZJBbe/3ek4/nR3A0oAVwZZMNGIH972P7cSZmb/uJXMOQKBgQCs\n" +
+                "FaQAL+4sN/TUxrkAkylqF+QJmEZ26l2nrzHZjMWROYNJcsn8/XkaEhD4vGSnazCu\n" +
+                "/B0vU6nMppmezF9Mhc112YSrw8QFK5GOc3NGNBoueqMYy1MG8Xcbm1aSMKVv8xba\n" +
+                "rh+BZQbxy6x61CpCfaT9hAoA6HaNdeoU6y05lBz1DQKBgAbYiIk56QZHeoZKiZxy\n" +
+                "4eicQS0sVKKRb24ZUd+04cNSTfeIuuXZrYJ48Jbr0fzjIM3EfHvLgh9rAZ+aHe/L\n" +
+                "84Ig17KiExe+qyYHjut/SC0wODDtzM/jtrpqyYa5JoEpPIaUSgPuTH/WhO3cDsx6\n" +
+                "3PIW4/CddNs8mCSBOqTnoaxh\n" +
+                "-----END PRIVATE KEY-----";
+    }
+
+    private static String loadRsaPublicKeyPem() {
+        // this is a sample key - don't worry !
+        return "-----BEGIN PUBLIC KEY-----\n" +
+                "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8EmWJUZ/Osz4vXtUU2S+\n" +
+                "0M4BP9+s423gjMjoX+qP1iCnlcRcFWxthQGN2CWSMZwR/vY9V0un/nsIxhZSWOH9\n" +
+                "iKzqUtZD4jt35jqOTeJ3PCSr48JirVDNLet7hRT37Ovfu5iieMN7ZNpkjeIG/CfT\n" +
+                "/QQl7R+kO/EnTmL3QjLKQNV/HhEbHS2/44x7PPoHqSqkOvl8GW0qtL39gTLWgAe8\n" +
+                "01/w5PmcQ38CKG0oT2gdJmJqIxNmAEHkatYGHcMDtXRBpOhOSdraFj6SmPyHEmLB\n" +
+                "ishaq7Jm8NPPNK9QcEQ3q+ERa5M6eM72PpF93g2p5cjKgyzzfoIV09Zb/LJ2aW2g\n" +
+                "QwIDAQAB\n" +
+                "-----END PUBLIC KEY-----";
+    }
+
+    public static PrivateKey getPrivateKeyFromString(String key) throws GeneralSecurityException {
+        String privateKeyPEM = key;
+        privateKeyPEM = privateKeyPEM.replace("-----BEGIN PRIVATE KEY-----", "");
+        privateKeyPEM = privateKeyPEM.replace("-----END PRIVATE KEY-----", "");
+        privateKeyPEM = privateKeyPEM.replaceAll("[\\r\\n]+", "");
+        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        PrivateKey privKey = (PrivateKey) kf.generatePrivate(keySpec);
+        return privKey;
+    }
+
+    public static PublicKey getPublicKeyFromString(String key) throws GeneralSecurityException {
+        String publicKeyPEM = key;
+        publicKeyPEM = publicKeyPEM.replace("-----BEGIN PUBLIC KEY-----", "");
+        publicKeyPEM = publicKeyPEM.replace("-----END PUBLIC KEY-----", "");
+        publicKeyPEM = publicKeyPEM.replaceAll("[\\r\\n]+", "");
+        byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PublicKey pubKey = (PublicKey) kf.generatePublic(new X509EncodedKeySpec(encoded));
+        return pubKey;
+    }
+
+    private static String loadRsaKeyPemFile(String filename) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
+    }
+}
---
+++ RsaEncryptionPkcs15String/RsaEncryptionPkcs15Xml.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+class RsaEncryptionOaepSha1 {
+	static void Main() {
+    Console.WriteLine("RSA 2048 encryption PKCS 1.5 string");
+    string dataToEncryptString = "The quick brown fox jumps over the lazy dog";
+    Console.WriteLine("plaintext: " + dataToEncryptString);
+
+    // # # # usually we would load the private and public key from a file or keystore # # #
+    // # # # here we use hardcoded keys for demonstration - don't do this in real programs # # #
+    string filenamePrivateKeyXml = "privatekey2048.xml";
+    string filenamePublicKeyXml = "publickey2048.xml";
+
+		try {
+      // encryption
+        Console.WriteLine("\n* * * encrypt the plaintext with the RSA public key * * *");
+        //string publicKeyLoad = loadRsaPublicKeyPem();
+        // use this in production
+        string publicKeyLoad = File.ReadAllText(filenamePublicKeyXml);
+        byte[] dataToEncrypt = Encoding.UTF8.GetBytes(dataToEncryptString);
+        string ciphertextBase64 = Base64Encoding(rsaEncryptionOaepSha1(publicKeyLoad, dataToEncrypt));
+        //string ciphertextBase64 = "";
+        Console.WriteLine("ciphertextBase64: " + ciphertextBase64);
+
+        // transport the encrypted data to recipient
+
+        // receiving the encrypted data, decryption
+        Console.WriteLine("\n* * * decrypt the ciphertext with the RSA private key * * *");
+        string ciphertextReceivedBase64 = ciphertextBase64;
+        Console.WriteLine("ciphertextReceivedBase64: " + ciphertextReceivedBase64);
+        //string privateKeyLoad = loadRsaPrivateKeyPem();
+        // use this in production
+        string privateKeyLoad = File.ReadAllText(filenamePrivateKeyXml);
+        byte[] ciphertextReceived = Base64Decoding(ciphertextReceivedBase64);
+        byte[] decryptedtextByte = rsaDecryptionOaepSha1(privateKeyLoad, ciphertextReceived);
+        Console.WriteLine("decryptedData: " + Encoding.UTF8.GetString(decryptedtextByte, 0, decryptedtextByte.Length));
+		}
+		catch(ArgumentNullException) {
+			Console.WriteLine("The data was not RSA encrypted");
+		}
+	}
+
+  public static byte[] rsaEncryptionOaepSha1(string publicKeyXml, byte[] plaintext) {
+    RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider(2048);
+		RSAalg.PersistKeyInCsp = false;
+		RSAalg.FromXmlString(publicKeyXml);
+		return RSAalg.Encrypt(plaintext, false);
+  }
+
+  public static byte[] rsaDecryptionOaepSha1(string privateKeyXml, byte[] ciphertext) {
+    RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider(2048);
+		RSAalg.PersistKeyInCsp = false;
+		RSAalg.FromXmlString(privateKeyXml);
+    return RSAalg.Decrypt(ciphertext, false);
+  }
+
+  static string Base64Encoding(byte[] input) {
+    return Convert.ToBase64String(input);
+  }
+
+  static byte[] Base64Decoding(String input) {
+    return Convert.FromBase64String(input);
+  }
+
+  public static string loadRsaPublicKeyPem() {
+    return "<RSAKeyValue><Modulus>8EmWJUZ/Osz4vXtUU2S+0M4BP9+s423gjMjoX+qP1iCnlcRcFWxthQGN2CWSMZwR/vY9V0un/nsIxhZSWOH9iKzqUtZD4jt35jqOTeJ3PCSr48JirVDNLet7hRT37Ovfu5iieMN7ZNpkjeIG/CfT/QQl7R+kO/EnTmL3QjLKQNV/HhEbHS2/44x7PPoHqSqkOvl8GW0qtL39gTLWgAe801/w5PmcQ38CKG0oT2gdJmJqIxNmAEHkatYGHcMDtXRBpOhOSdraFj6SmPyHEmLBishaq7Jm8NPPNK9QcEQ3q+ERa5M6eM72PpF93g2p5cjKgyzzfoIV09Zb/LJ2aW2gQw==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+  }
+
+  public static string loadRsaPrivateKeyPem() {
+    return "<RSAKeyValue><Modulus>8EmWJUZ/Osz4vXtUU2S+0M4BP9+s423gjMjoX+qP1iCnlcRcFWxthQGN2CWSMZwR/vY9V0un/nsIxhZSWOH9iKzqUtZD4jt35jqOTeJ3PCSr48JirVDNLet7hRT37Ovfu5iieMN7ZNpkjeIG/CfT/QQl7R+kO/EnTmL3QjLKQNV/HhEbHS2/44x7PPoHqSqkOvl8GW0qtL39gTLWgAe801/w5PmcQ38CKG0oT2gdJmJqIxNmAEHkatYGHcMDtXRBpOhOSdraFj6SmPyHEmLBishaq7Jm8NPPNK9QcEQ3q+ERa5M6eM72PpF93g2p5cjKgyzzfoIV09Zb/LJ2aW2gQw==</Modulus><Exponent>AQAB</Exponent><P>/8atV5DmNxFrxF1PODDjdJPNb9pzNrDF03TiFBZWS4Q+2JazyLGjZzhg5Vv9RJ7VcIjPAbMy2Cy5BUffEFE+8ryKVWfdpPxpPYOwHCJSw4Bqqdj0Pmp/xw928ebrnUoCzdkUqYYpRWx0T7YVRoA9RiBfQiVHhuJBSDPYJPoP34k=</P><Q>8H9wLE5L8raUn4NYYRuUVMa+1k4Q1N3XBixm5cccc/Ja4LVvrnWqmFOmfFgpVd8BcTGaPSsqfA4j/oEQp7tmjZqggVFqiM2mJ2YEv18cY/5kiDUVYR7VWSkpqVOkgiX3lK3UkIngnVMGGFnoIBlfBFF9uo02rZpC5o5zebaDIms=</Q><DP>BPXecL9Pp6u/0kwY+DcCgkVHi67J4zqka4htxgP04nwLF/o8PF0tlRfj0S7qh4UpEIimsxq9lrGvWOne6psYxG5hpGxiQQvgIqBGLxV/U2lPKEIb4oYAOmUTYnefBCrmSQW3v93pOP50dwNKAFcGWTDRiB/e9j+3EmZm/7iVzDk=</DP><DQ>rBWkAC/uLDf01Ma5AJMpahfkCZhGdupdp68x2YzFkTmDSXLJ/P15GhIQ+Lxkp2swrvwdL1OpzKaZnsxfTIXNddmEq8PEBSuRjnNzRjQaLnqjGMtTBvF3G5tWkjClb/MW2q4fgWUG8cusetQqQn2k/YQKAOh2jXXqFOstOZQc9Q0=</DQ><InverseQ>BtiIiTnpBkd6hkqJnHLh6JxBLSxUopFvbhlR37Thw1JN94i65dmtgnjwluvR/OMgzcR8e8uCH2sBn5od78vzgiDXsqITF76rJgeO639ILTA4MO3Mz+O2umrJhrkmgSk8hpRKA+5Mf9aE7dwOzHrc8hbj8J102zyYJIE6pOehrGE=</InverseQ><D>hXGYfOMFzXX/vds8HYQZpISDlSF3NmbTCdyZkIsHjndcGoSOTyeEOxV93MggxIRUSjAeKNjPVzikyr2ixdHbp4fAKnjsAjvcfnOOjBp09WW4QCi3/GCfUh0w39uhRGZKPjiqIj8NzBitN06LaoYD6MPg/CtSXiezGIlFn/Hs+MuEzNFu8PFDj9DhOFhfCgQaIgEEr+IHdnl5HuUVrwTnIBrEzZA/08Q0Gv86qQZctZWoD9hPGzeAC+RSMyGVJw6Ls8zBFf0eysB4spsu4LUom/WnZMdS1ls4eqsAX+7AdqPKBRuUVpr8FNyRM3s8pJUiGns6KFsPThtJGuH6c6KVwQ==</D></RSAKeyValue>";
+  }
+}
